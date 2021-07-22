50dbfd4f82c4da08a48d12df3544b72a811e2c13
Espadaadelino <mespadadelino@gmail.com>
2021-07-22 12:07:20 +0200

    routing map done

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LeafletMap from "./components/Maps/LeafletMap";
+
+function App() {
+    return (
+        <div>
+            <LeafletMap/>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/Maps/RoutingMachine.js
@@ -0,0 +1,20 @@
+import {MapLayer} from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import {withLeaflet} from "react-leaflet";
+
+class Routing extends MapLayer {
+    createLeafletElement() {
+        const {map} = this.props;
+        let leafletElement = L.Routing.control({
+            waypoints: [
+                L.latLng(40.420625, -3.692291),
+                L.latLng(40.3966428, -3.6822935),
+                L.latLng(40.4146500, -3.7004000)
+            ],
+            language: 'es'
+        }).addTo(map.leafletElement);
+        return leafletElement.getPlan();
+    }
+}
+export default withLeaflet(Routing);
