fd595d678b66dfa7483adcbb50b56211c71ef1a3
Espadaadelino <mespadadelino@gmail.com>
2021-07-26 18:10:15 +0200

    header y material ui

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Home from '../../pages/home'
+import Home from '../../pages/Home/home'
 import Maps from '../../pages/map'
 import Explora from '../../pages/explora'
 import Actividades from '../../pages/actividades'
 import './Main.css';
 
-const Main= () =>{
-    
-        return (
-            <div className='Main'>
-                {/* <div className="wrapper"> */}
-                    <Switch>
-                        <Route path="/" component={Home} exact />
-                        <Route path="/rutas" component={Maps} />
-                        <Route path="/explora" component={Explora} />
-                        <Route path="/actividades" component={Actividades} />
-                    </Switch>
-                {/* </div> */}
-            </div>
-        )
-    }
+const Main = () => {
+
+    return (
+        <div className='Main'>
+            {/* <div className="wrapper"> */}
+            <Switch>
+                <Route path="/"
+                    component={Home}
+                    exact/>
+                <Route path="/rutas"
+                    component={Maps}/>
+                <Route path="/explora"
+                    component={Explora}/>
+                <Route path="/actividades"
+                    component={Actividades}/>
+            </Switch>
+            {/* </div> */} </div>
+    )
+}
 
 
 export default Main;
---
+++ client/src/pages/Home/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import './home.css'
+
+
+function home() {
+    return (
+        <div>
+            <div>
+
+                <h5>Descrubre</h5>
+                <h2>Barcelona</h2>
+            </div>
+            <Header/>
+
+        </div>
+    )
+}
+
+export default home
