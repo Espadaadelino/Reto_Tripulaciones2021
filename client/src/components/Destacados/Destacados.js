bc3e4dfa4e659471b9b2b71dbc95f5520afd93c2
Espadaadelino <mespadadelino@gmail.com>
2021-07-29 16:02:59 +0200

    css tarjetas

--- client/src/components/Destacados/Destacados.js
+++ client/src/components/Destacados/Destacados.js
@@ -24,104 +24,99 @@ export default function RecipeReviewCard() {
     const classes = useStyles();
 
     return (
-        <div class="carousel">
 
-            <AliceCarousel autoWidth={true}
-                mouseTracking
-                disableButtonsControls
-                disableDotsControls>
-                <Card>
-                    <CardMedia className={
-                            classes.media
+
+        <AliceCarousel autoWidth={true}
+            mouseTracking
+            disableButtonsControls
+            disableDotsControls>
+
+            <Card>
+                <CardMedia className={
+                        classes.media
+                    }
+                    image="https://media.timeout.com/images/105737732/image.jpg"/>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        <h2>La Sagrada Familia</h2>
+                        La Sagrada Familia, es una basílica católica de Barcelona (España),diseñada por el arquitecto Antoni Gaudí. Iniciada en 1882. Cuando esté finalizada será la iglesia cristiana más alta del mundo. Es uno de los monumentos más visitados de España, y es la iglesia más visitada de Europa tras la basílica de San Pedro del Vaticano.
+                    </Typography>
+
+                    <AccessibleIcon style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
                         }
-                        image="https://media.timeout.com/images/105737732/image.jpg"/>
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            <h2>La Sagrada Familia</h2>
-                            La Sagrada Familia, es una basílica católica de Barcelona (España),
-                                                                                                                                                                                                                                                                                                                                                                            diseñada por el arquitecto Antoni Gaudí. Iniciada en 1882, todavía
-                                                                                                                                                                                                                                                                                                                                                                            está en construcción.
-                        </Typography>
-
-                        <AccessibleIcon style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
-                        <Rating style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
-
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardMedia className={
-                            classes.media
+                    }/>
+                    <Rating style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
                         }
-                        image="https://v2r8c2i8.stackpathcdn.com/wp-content/uploads/2015/05/praiadebarcelonetaembarcelona.jpg"/>
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            <h2>Playa de la Barceloneta</h2>
-                            Se encuentra en el barrio de La Barceloneta (Ciutat Vella). Junto
-                                                                                                                                                                                                                                                                                                                                                                            con la barcelonesa playa de San Sebastián y la playa de la Victoria,
-                                                                                                                                                                                                                                                                                                                                                                            es una de las más largas con una longitud de 1100 metros. Junto con
-                                                                                                                                                                                                                                                                                                                                                                            la de San Sebastián (playa cercana) es una de las más antiguas y de
-                                                                                                                                                                                                                                                                                                                                                                            mayor tradición de la ciudad y también una de las preferidas por los
-                                                                                                                                                                                                                                                                                                                                                                            usuarios extranjeros.
-                        </Typography>
+                    }/>
 
-                        <AccessibleIcon style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
-                        <Rating style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
+                </CardContent>
+            </Card>
 
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardMedia className={
-                            classes.media
+
+            <Card>
+                <CardMedia className={
+                        classes.media
+                    }
+                    image="https://v2r8c2i8.stackpathcdn.com/wp-content/uploads/2015/05/praiadebarcelonetaembarcelona.jpg"/>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        <h2>Playa de la Barceloneta</h2>
+                        La playa de la Barceloneta es la más antigua y famosa de las playas de la ciudad de Barcelona (España). Fue remodelada justo antes de los Juegos Olímpicos de 1992.Se encuentra en el barrio de La Barceloneta (Ciutat Vella).
+
+                    </Typography>
+
+                    <AccessibleIcon style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
                         }
-                        image="https://upload.wikimedia.org/wikipedia/commons/3/3f/Arc_Triomf_31-10-13.JPG"/>
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            <h2>El Arco del Triunfo</h2>
-                            Construido con motivo de la Exposición Universal de 1888, el Arco de
-                                                                                                                                                                                                                                                                                                                                                                            Triunfo encabezaba el paseo que conducía hasta la entrada principal
-                                                                                                                                                                                                                                                                                                                                                                            de la feria, en el parque de la Ciutadella, que se acababa de
-                                                                                                                                                                                                                                                                                                                                                                            inaugurar. El monumento, de ladrillo rojo y de estilo neomudéjar,
-                                                                                                                                                                                                                                                                                                                                                                            respondía al gusto por la ornamentación y el eclecticismo tan
-                                                                                                                                                                                                                                                                                                                                                                            característico del modernismo catalán, en auge en aquellos años.
-                        </Typography>
+                    }/>
+                    <Rating style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
+                        }
+                    }/>
 
-                        <AccessibleIcon style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
-                        <Rating style={
-                            {
-                                fontSize: 15,
-                                color: "blue"
-                            }
-                        }/>
+                </CardContent>
+            </Card>
 
-                    </CardContent>
-                </Card>
-            </AliceCarousel>
-        </div>
+
+            <Card>
+                <CardMedia className={
+                        classes.media
+                    }
+                    image="https://upload.wikimedia.org/wikipedia/commons/3/3f/Arc_Triomf_31-10-13.JPG"/>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        <h2>El Arco del Triunfo</h2>
+                        Construido con motivo de la Exposición Universal de 1888, el Arco del Triunfo encabezaba el paseo que conducía hasta la entrada principal de la feria, en el parque de la Ciutadella, que se acababa deinaugurar. El monumento, de ladrillo rojo y de estilo neomudéjar,respondía al gusto por la ornamentación y el eclecticismo.
+                    </Typography>
+
+                    <AccessibleIcon style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
+                        }
+                    }/>
+                    <Rating style={
+                        {
+                            fontSize: 15,
+                            color: "blue"
+                        }
+                    }/>
+
+                </CardContent>
+            </Card>
+
+        </AliceCarousel>
+
 
     );
 }
